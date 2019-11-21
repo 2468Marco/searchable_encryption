cc9f9c43deaeedd643ddc8b94249e2cb0006461b
sethdiehm <diehmsr@mail.uc.edu>
2019-11-20 22:46:16 -0500

    Part 1 and 2 done

--- src/se.py
+++ src/se.py
@@ -1,37 +1,142 @@
 import os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
 
+### INDEX CREATION AND MANIPULATION 
 def get_index():
     index = {}
     directory = '../data/files'
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
-            print(filename)
             path = directory + '/' + filename
             f = open(path, "r")
             keywords = f.read()
-            print (keywords)
             f.close()
             index[filename] = keywords
             continue
         else:
             continue
-    print(index)
     return index
 
 def invert_index(index):
     inverted_index = {}
     for txt in index:
-        print(txt)
-        keyword_list = index[txt].split(" ")
-        for keyword in keyword_list:
+        for keyword in index[txt]:
             if (keyword not in inverted_index):
                 temp_list = []
                 temp_list.append(txt)
                 inverted_index[keyword] = temp_list
-                print(inverted_index)
                 continue
             else:
                 inverted_index[keyword].append(txt)
-                print(inverted_index)
                 continue
     return inverted_index
+
+
+### KEY GENERATION FUNCTIONS
+#key_make takes in a integer and returns a hexidecimal string with the same length
+def key_make(key_length):
+        #Gets a hexidecimal that uses the number of bytes given)
+        key=get_random_bytes(key_length)
+        #print(key.hex())
+        return key.hex()
+
+#key_save saves the key inputed to the file key.txt
+def key_save(key,file):
+        f = open(file,"w+")
+        f.write(key)
+        f.close
+
+#keyGen creates 2 'l' bit key and then saves them to skprf.txt and skaes.txt
+#'l' should be 16,24,32
+def keys_gen(l):
+        key = key_make(l)
+        key_save(key,"../data/skprf.txt")
+        key = key_make(l)
+        key_save(key,"../data/skaes.txt")
+
+
+### STRING MANIPULATION
+#converts strings to hexadecimal format
+def strToHex(string):
+        binary_string = ""
+        for x in string:
+                one_letter_ASCII = ord(x)
+                one_letter_binary = bin(one_letter_ASCII)
+                one_letter_binary = one_letter_binary[2::]
+                #Padding front zeros
+                while (len(one_letter_binary) != 8):
+                        one_letter_binary = "0"+one_letter_binary
+                binary_string += one_letter_binary
+        hex_string = hex(int(binary_string,2))
+        return hex_string[2::]
+
+def enc():
+    #get the plaintext index
+    index = get_index()
+
+    #get skprf
+    f = open("../data/skprf.txt","r")
+    if f.mode == "r":
+        sk = f.read()
+        #print("secret key prf:    "+sk)
+    f.close()
+    prfKey = bytes
+    prfKey = prfKey.fromhex(sk)
+
+    #get skaes
+    f = open("../data/skaes.txt","r")
+    if f.mode == "r":
+        sk = f.read()
+        #print("secret key aes:    "+sk)
+    f.close()
+    aesKey = bytes
+    aesKey = aesKey.fromhex(sk)
+
+    ## make ciphers
+    #keywords cipher
+    prfCipher = AES.new(prfKey,AES.MODE_ECB)
+    #files cipher
+    aesCipher = AES.new(aesKey,AES.MODE_CBC)
+
+    #preperation for indexing
+    byteText = bytes
+    encIndex = {}
+    i=0
+
+    #for every document in the plaintext index
+    for doc in index:
+        #encrypting the file text
+        #generate filename/path
+        i+=1
+        fileName = "c"+str(i)+".txt"
+        #NOTE THE FOLDER 'ciphertextfiles' MUST BE PREEXISTING
+        filepath = "../data/ciphertextfiles/"+fileName
+
+        #encrypting the file
+        f = open(filepath,"w")
+        #the encrypt funciton does not use 'str' objects so it is nessassary to change it to hex format
+        hexText = strToHex(index[doc])
+        byteText = byteText.fromhex(hexText)
+        #pad the text so that it fills the block
+        padmsg = pad(byteText,16)
+        fileEnc = aesCipher.encrypt(padmsg)
+        f.write(fileEnc.hex())
+        f.close()
+
+        #encrypting the keywords
+        keyword_list = index[doc].split(" ")
+        encIndex[fileName] = []
+        for keyword in keyword_list:
+            hexText = strToHex(keyword)
+            byteText = byteText.fromhex(hexText)
+            padmsg = pad(byteText,16)
+            wordEnc = prfCipher.encrypt(padmsg)
+            #print(wordEnc.hex())
+            encIndex[fileName].append(wordEnc.hex())
+    #inverting the encrypted index
+    invertedEncIndex = invert_index(encIndex)
+    f = open("../data/index.txt","w")
+    f.write(str(invertedEncIndex))
+    f.close()
